3a9bd62e8b3a24005a34d9cfc47041d0f3fc6ff7
Vellmond <edgar.bautista@edu.uag.mx>
2024-08-13 11:21:47 -0600

    first commit

---
+++ src/main/java/com/vellmond/dragonball/VellmondDragonballClientApplication.java
@@ -0,0 +1,13 @@
+package com.vellmond.dragonball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VellmondDragonballClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VellmondDragonballClientApplication.class, args);
+    }
+
+}
